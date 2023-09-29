c3b9aeb93120c6145b4d4bbc68c3bc91d1be5fa0
rudoy <rudoy@nca.by>
2023-09-29 17:44:13 +0300

    fix: settings

--- node/src/main/java/com/rudoy/config/RabbitConfiguration.java
+++ node/src/main/java/com/rudoy/config/RabbitConfiguration.java
@@ -1,12 +1,9 @@
 package com.rudoy.config;
 
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static com.rudoy.RabbitQueue.*;
 
 @Configuration
 public class RabbitConfiguration {
