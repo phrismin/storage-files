2b0986fcc6940965e9425cddfd622e607b058f9f
rudoy <rudoy@nca.by>
2023-09-29 17:44:28 +0300

    fix: settings2

---
+++ node/src/main/java/com/rudoy/service/impl/MainServiceImpl.java
@@ -0,0 +1,39 @@
+package com.rudoy.service.impl;
+
+import com.rudoy.dao.RawDataDAO;
+import com.rudoy.entity.RawData;
+import com.rudoy.service.MainService;
+import com.rudoy.service.ProducerService;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class MainServiceImpl implements MainService {
+    private final RawDataDAO rawDataDAO;
+    private final ProducerService producerService;
+
+    public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService) {
+        this.rawDataDAO = rawDataDAO;
+        this.producerService = producerService;
+    }
+
+    @Override
+    public void processTextMessage(Update update) {
+        saveRawData(update);
+
+        var message = update.getMessage();
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText("Hello from NODE");
+        producerService.producerAnswer(sendMessage);
+    }
+
+    private void saveRawData(Update update) {
+        RawData rawData = RawData.builder()
+                .event(update)
+                .build();
+        rawDataDAO.save(rawData);
+    }
+
+}
